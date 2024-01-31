b1b47d49b541a31c6608b6f742f7af5c05ac5304
RajveerSehmbi <rajveer.sehmbi@outlook.com>
2024-01-31 11:45:19 +0000

    Added filter POST endpoint on api.

--- app/app.py
+++ app/app.py
@@ -4,10 +4,28 @@ from data.books import books
 
 app = Flask(__name__)
 
-@app.route('/books', methods=['GET'])
-def bookList():
-    return jsonify(books)
+@app.route('/books', methods=['POST'])
+def index():
+    if request.method == 'POST':
+        # Get form data
+        title = request.form.get('title')
+        author = request.form.get('author')
+        publication_year = request.form.get('publication_year')
+        genre = request.form.get('genre')
 
+        # Filter books based on form data
+        filtered_books = books
+        if title:
+            filtered_books = [book for book in filtered_books if book['title'] == title]
+        if author:
+            filtered_books = [book for book in filtered_books if book['author'] == author]
+        if publication_year:
+            filtered_books = [book for book in filtered_books if book['publication_year'] == int(publication_year)]
+        if genre:
+            filtered_books = [book for book in filtered_books if book['genre'] == genre]
+
+        # Return the filtered books as JSON
+        return jsonify(filtered_books)
 
 if __name__ == '__main__':
     app.run(debug=True)
